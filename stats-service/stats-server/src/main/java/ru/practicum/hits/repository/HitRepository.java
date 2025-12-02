e9e03d65275fc5acb7baeedd6036e4824c7ee76c
Stepan Belenov <Sabaneev99@yandex.ru>
2025-12-02 21:19:58 +0300

    Диплом 1ч

--- stats-service/stats-server/src/main/java/ru/practicum/hits/repository/HitRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/hits/repository/HitRepository.java
@@ -13,27 +13,31 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM Hit h WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> findUniqueHitsUris(@Param("start") LocalDateTime start,
                                           @Param("end") LocalDateTime end,
                                           @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM Hit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> findUniqueHits(@Param("start") LocalDateTime start,
                                       @Param("end") LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h)) " +
             "FROM Hit h WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h) DESC")
     List<ViewStatsDto> findHitsUris(@Param("start") LocalDateTime start,
                                     @Param("end") LocalDateTime end,
                                     @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h)) " +
             "FROM Hit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h) DESC")
     List<ViewStatsDto> findHits(@Param("start") LocalDateTime start,
                                 @Param("end") LocalDateTime end);
 
